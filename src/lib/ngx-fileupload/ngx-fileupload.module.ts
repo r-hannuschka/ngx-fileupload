c01c36babfc4332a743edb030f1afde5e81e2322
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2019-06-29 01:23:45 +0200

    update configurations

--- src/lib/ngx-fileupload/ngx-fileupload.module.ts
+++ src/lib/ngx-fileupload/ngx-fileupload.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { UploadItemComponent } from './components/upload-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BytesPipe } from 'angular-pipes';
+import { NgMathPipesModule } from 'angular-pipes';
 import { NgxFileuploadDirective } from './directives/ngx-fileuplad';
 import { UploadComponent } from './components/upload.component';
 
@@ -12,12 +12,12 @@ import { UploadComponent } from './components/upload.component';
         NgxFileuploadDirective,
         UploadItemComponent,
         UploadComponent,
-        BytesPipe
     ],
     imports: [
         CommonModule,
         HttpClientModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        NgMathPipesModule
     ],
     exports: [
         NgxFileuploadDirective,

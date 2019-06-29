ecf853ff7c2362194a39ab0f90a611561fa26463
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2019-06-29 16:48:19 +0200

    add / implement icon font, styles

--- src/lib/ngx-fileupload/ngx-fileupload.module.ts
+++ src/lib/ngx-fileupload/ngx-fileupload.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { UploadItemComponent } from './components/upload-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgMathPipesModule } from 'angular-pipes';
+
+import { UploadItemComponent } from './components/upload-item.component';
 import { NgxFileuploadDirective } from './directives/ngx-fileuplad';
 import { UploadComponent } from './components/upload.component';
 

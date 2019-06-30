f95cf6eebf7d0b977d76fbc18ed0796d944fc60e
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2019-06-30 14:07:49 +0200

    fixed public api

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -4,3 +4,6 @@
 export * from './ngx-fileupload/model/upload';
 export * from './ngx-fileupload/services/file-upload';
 export * from './ngx-fileupload/ngx-fileupload.module';
+export * from './ngx-fileupload/directives/ngx-fileuplad';
+export * from './ngx-fileupload/components/upload-item.component';
+export * from './ngx-fileupload/components/upload.component';

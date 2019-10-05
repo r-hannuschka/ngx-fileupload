38206a4dd2d724e2de2449c085bad41960e02462
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-10-06 01:59:51 +0200

    [FR - #85] - update demo page

--- src/example/libs/pages/item-template/src/circle-progressbar/circle-progressbar.component.ts
+++ src/example/libs/pages/item-template/src/circle-progressbar/circle-progressbar.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Upload, UploadState } from "@r-hannuschka/ngx-fileupload";
 
 @Component({
     selector: "app-item-template--circle-progressbar",
     templateUrl: "circle-progressbar.component.html"
 })
 
-export class CircleProgressbarComponent implements OnInit {
-    constructor() { }
+export class CircleProgressbarComponent {
 
-    ngOnInit() { }
+    public uploads: Upload[] = [];
+
+    public onUploadAdd(uploads: Upload[]) {
+        this.uploads = [...this.uploads, ...uploads];
+    }
+
+    public stateChanged(changedUpload: Upload) {
+
+        let uploadDone = changedUpload.data.state === UploadState.CANCELED;
+        uploadDone     = uploadDone || changedUpload.data.state === UploadState.UPLOADED;
+
+        /**
+         * remove upload from list
+         */
+        if (uploadDone) {
+            this.uploads = this.uploads.filter((upload) => changedUpload !== upload);
+        }
+    }
 }

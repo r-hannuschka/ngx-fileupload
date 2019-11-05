b42fa90a9fc614067d45ace68ed89ae14b97191a
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-11-05 22:54:33 +0100

    create example page usage ngx-file-drop library

--- src/example/libs/data/code/ngx-drop-zone/drop-zone.ts
+++ src/example/libs/data/code/ngx-drop-zone/drop-zone.ts
@@ -1,10 +1,9 @@
+export const TS = `
 import { Component, Inject, OnInit, OnDestroy } from "@angular/core";
 import { UploadStorage, NgxFileUploadFactory, UploadOptions, UploadRequest, UploadApi } from "@r-hannuschka/ngx-fileupload";
 import { NgxFileDropEntry, FileSystemFileEntry } from "ngx-file-drop";
 import { takeUntil, tap } from "rxjs/operators";
 import { Subject } from "rxjs";
-
-import * as ExampleCodeData from "@ngx-fileupload-example/data/code/ngx-drop-zone/drop-zone";
 
 @Component({
     selector: "app-drop-zone",
@@ -17,8 +16,7 @@ export class DropZoneComponent implements OnDestroy, OnInit {
 
     public uploadStorage: UploadStorage;
 
-    public code = ExampleCodeData;
-
+    /** UploadState enum so we could use this in template */
     public states = UploadApi.UploadState;
 
     /** upload options */
@@ -86,3 +84,47 @@ export class DropZoneComponent implements OnDestroy, OnInit {
         this.uploadStorage = null;
     }
 }
+`;
+
+export const HTML = `
+<!-- SECTION TEMPLATES -->
+<ng-template #itemTemplate let-upload="data" let-ctrl="ctrl">
+    <div class="upload-item">
+        <div class="label"><p>{{upload.name}}</p></div>
+
+        <!-- bootstrap progressbar -->
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" [ngStyle]="{width: upload.progress + '%'}"></div>
+        </div>
+
+        <div class="actions" [ngSwitch]="upload.state">
+            <!-- only icons -->
+            <span class="state" *ngSwitchCase="states.PENDING"><i class="icon-pending"></i></span>
+            <span class="state" *ngSwitchCase="states.START"><i class="icon-connect"></i></span>
+            <span class="state" *ngSwitchCase="states.PROGRESS"><i class="icon-progress"></i></span>
+            <span class="state" *ngSwitchCase="states.COMPLETED">
+                <i [ngClass]="upload.hasError ? 'icon-warning' : 'icon-success'"></i>
+            </span>
+
+            <!-- action buttons -->
+            <button type="button" class="btn state" *ngSwitchDefault (click)="ctrl.start()">
+                <i class="icon-upload"></i>
+            </button>
+            <button type="button" class="btn state" (click)="ctrl.remove()"><i class="icon-cancel"></i></button>
+        </div>
+    </div>
+</ng-template>
+
+<ngx-fileupload-toolbar [storage]="uploadStorage"></ngx-fileupload-toolbar>
+
+<ngx-file-drop (onFileDrop)="drop($event)"
+    [dropZoneLabel]="'Drop or'"
+    [dropZoneClassName]="'ngx-fileupload__ngx-file-drop'"
+    [showBrowseBtn]="true"
+    [browseBtnLabel]="'Browse'">
+</ngx-file-drop>
+
+<div class="files">
+    <ngx-fileupload-item *ngFor="let upload of uploads" [template]="itemTemplate" [upload]="upload"></ngx-fileupload-item>
+</div>
+`;

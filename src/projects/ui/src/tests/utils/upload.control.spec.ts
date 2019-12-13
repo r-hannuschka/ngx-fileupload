38549539ef2a1d3cda8e485aff7e300dc85fd6ec
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-12-13 21:21:28 +0100

    add upload.control unit test again

---
+++ src/projects/ui/src/tests/utils/upload.control.spec.ts
@@ -0,0 +1,42 @@
+import { UploadRequest } from "@ngx-file-upload/core";
+import { Control } from "@ngx-file-upload/dev/ui/public-api";
+
+import { fakeAsync, tick } from "@angular/core/testing";
+describe("ngx-fileupload/libs/upload/upload-control", () => {
+
+    let fileUpload: UploadRequest;
+    let uploadCtrl: Control;
+
+    beforeEach(() => {
+        fileUpload = jasmine.createSpyObj("UploadRequest", {
+            retry: () => {},
+            start: () => {},
+            cancel: () => {},
+            destroy: () => {}
+        });
+        uploadCtrl = new Control(fileUpload);
+    });
+
+    it ("should call retry", () => {
+        uploadCtrl.retry();
+        expect(fileUpload.retry).toHaveBeenCalled();
+    });
+
+    it ("should call start", () => {
+        uploadCtrl.start();
+        expect(fileUpload.start).toHaveBeenCalled();
+    });
+
+    it ("should call stop", fakeAsync(() => {
+        uploadCtrl.stop();
+        tick(0);
+        expect(fileUpload.cancel).toHaveBeenCalled();
+    }));
+
+    it ("should call remove", fakeAsync(() => {
+        const event = new MouseEvent("click");
+        uploadCtrl.remove(event);
+        tick(0);
+        expect(fileUpload.destroy).toHaveBeenCalled();
+    }));
+});

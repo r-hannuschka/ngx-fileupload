e5f9c3af3e0c97f74f7586e622bb666d5d901f2c
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-12-14 19:55:30 +0100

    fix(ui): remove barrel imports

--- src/projects/ui/src/lib/ui.module.ts
+++ src/projects/ui/src/lib/ui.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { UploadViewComponent, FileBrowserDirective, UploadItemComponent, UploadToolbarComponent } from "./ui";
-import { FileSizePipe, StateToStringPipe, CancelAblePipe } from "./utils";
+import { FileBrowserDirective } from './ui/src/file-browser';
+import { UploadItemComponent } from './ui/src/upload-item.component';
+import { UploadToolbarComponent } from './ui/src/upload-toolbar';
+import { UploadViewComponent } from './ui/src/upload-view';
+
+import { CancelAblePipe } from "./utils/src/cancelable.pipe";
+import { FileSizePipe } from "./utils/src/file-size.pipe";
+import { StateToStringPipe } from "./utils/src/state-to-string.pipe";
 
 @NgModule({
     declarations: [

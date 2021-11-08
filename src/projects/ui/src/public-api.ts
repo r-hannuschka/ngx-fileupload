7acc10932036aa96e6f76595096a99ea3b2ac9f6
Ralf Hannuschka <r.hannuschka@gmail.com>
2021-11-08 11:50:42 +0100

    feat: upgrade to angular 13 (#616)
    * feat: upgrade to angular 13
    
    * fix: errors
    
    * fix: code coverage
    
    * update github workflow for core

--- src/projects/ui/src/public-api.ts
+++ src/projects/ui/src/public-api.ts
@@ -5,10 +5,21 @@ export * from "./lib/ui.module";
 
 export * from "./lib/common/main";
 export * from "./lib/file-browser/main";
-export * from "./lib/progressbar/main";
-export * from "./lib/toolbar/main";
-export * from "./lib/upload-item/main";
+export * from "./lib/file-browser/src/file-browser";
 
+export * from "./lib/progressbar/main";
+export * from "./lib/progressbar/src/ui/progressbar";
+export * from "./lib/progressbar/src/ui/progressbar-circle";
+
+export * from "./lib/toolbar/main";
+export * from "./lib/toolbar/src/toolbar";
+
+export * from "./lib/upload-item/main";
+export * from "./lib/upload-item/src/upload-item";
 export * from "./lib/upload-item/src/upload.control";
+
+export * from "./lib/common/src/cancelable.pipe"
+export * from "./lib/common/src/file-size.pipe"
+export * from "./lib/common/src/state-to-string.pipe"
 
 export { NgxFileUploadUiI18n, NGX_FILE_UPLOAD_UI_I18N } from "./lib/i18n";
